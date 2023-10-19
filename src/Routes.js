0e0feed5e3384b1045dbeda8807201fb300f2f04
MizoDaw <moazdawalibi@yahoo.com>
2023-10-19 13:22:16 +0300

    fix translate bug

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,8 @@
 import React, { Suspense } from "react";
 import { Routes, BrowserRouter as Router, Route } from "react-router-dom";
 import MainPage from "./sections/Main/MainSection";
+import i18n from '../src/translate/Translate'
+
 
 export default function AppRoutes() {
 
