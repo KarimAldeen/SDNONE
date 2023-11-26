460622920172488cb80ca8e7f99c3980051f0728
MizoDaw <moazdawalibi@yahoo.com>
2023-11-26 15:54:40 +0300

    .

--- src/config/LogoConfig.js
+++ src/config/LogoConfig.js
@@ -14,20 +14,25 @@ const LogoConfig = () => {
 
         <OurPartner
           image1={"/assets/OurPartner/4.png"}
-          image2={"/assets/OurPartner/5.jpg"}
+          image2={"/assets/OurPartner/whiteLogo.png"}
           image3={"/assets/OurPartner/6.png"}
           />
 
         <OurPartner
           image1={"/assets/OurPartner/7.jpg"}
-          image2={"/assets/OurPartner/10.png"}
+          image2={"/assets/OurPartner/5.jpg"}
           image3={"/assets/OurPartner/9.jpg"}
           />
 
         <OurPartner
           image1={"/assets/OurPartner/11.png"}
-          image2={"/assets/OurPartner/8.png"}
+          image2={"/assets/OurPartner/whiteLogo.png"}
           image3={"/assets/OurPartner/7.png"}
+          />
+          <OurPartner
+          image1={"/assets/OurPartner/12.png"}
+          image2={"/assets/OurPartner/8.png"}
+          image3={"/assets/OurPartner/10.png"}
           />
 
     </div>
